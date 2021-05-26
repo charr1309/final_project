dff4ee68004292269cbef6e8b62c8063915aa386
charr1309 <ceh710@aol.com>
2021-05-26 03:20:26 -0400

    finished adding content to the rest of the pages

--- final_project/src/views/Newsletter.js
+++ final_project/src/views/Newsletter.js
@@ -1,2 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Newsletter extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Custom Flyers, Newsletters, Folders, Etc.</h1>
+                <h2>We're more than just postcards. We can help you create a custom flyer, newsletter, folder, etc. To the right is one of the customized pocket folders we've done - stay tuned for more samples.</h2>
+                <p>Order Now - By phone - 404.849.6139 By email - info@mrpostcard.com</p>
+                <p>Pricing: Click here.</p>
+            </div>
+        )
+    }
+}
 
 
